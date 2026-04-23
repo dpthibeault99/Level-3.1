eca7af50631c3e1077ab091e0460260fa0f9f24e
dpthibeault99 <dpthibeault@email.neit.edu>
2026-04-23 17:03:02 -0400

    player 2 started

---
+++ js/controls.js
@@ -0,0 +1,96 @@
+var a = false;
+var d = false;
+var w = false;
+var s = false;
+var left = false;
+var right = false;
+var up = false;
+var down = false;
+
+// Up Arrow = 38
+// Down Arrow = 40
+// Left Arrow = 37
+// Right Arrow = 39
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+
+function press(e)
+{
+    console.log("Pressed " + e.keyCode);
+    // WASD
+    if (e.keyCode == 65)
+    {
+        a=true;
+    }
+    if (e.keyCode == 68)
+    {
+        d=true;
+    }
+    if (e.keyCode == 87)
+    {
+        w=true;
+    }
+    if (e.keyCode == 83)
+    {
+        s=true;
+    }
+    // Arrow Keys
+    if (e.keyCode == 38)
+    {
+        up=true;
+    }
+    if (e.keyCode == 40)
+    {
+        down=true;
+    }
+    if (e.keyCode == 37)
+    {
+        left=true;
+    }
+    if (e.keyCode == 39)
+    {
+        right=true;
+    }
+}
+
+function release(e)
+{
+    console.log("Pressed " + e.keyCode);
+    // WASD
+    if (e.keyCode == 65)
+    {
+        a=false;
+    }
+    if (e.keyCode == 68)
+    {
+        d=false;
+    }
+     if (e.keyCode == 87)
+    {
+        w=false;
+    }
+    if (e.keyCode == 83)
+    {
+        s=false;
+    }
+    // Arrow Keys
+    if (e.keyCode == 38)
+    {
+        up=false;
+    }
+    if (e.keyCode == 40)
+    {
+        down=false;
+    }
+    if (e.keyCode == 37)
+    {
+        left=false;
+    }
+    if (e.keyCode == 39)
+    {
+        right=false;
+    }
+}
+
---
+++ js/game.js
@@ -0,0 +1,98 @@
+var canvas;
+var context;
+var player;
+var player2;
+var ball;
+var timer;
+var interval = 1000 / 60; // 60 fps
+
+canvas = document.getElementById("myCanvas");
+context = canvas.getContext("2d");
+
+// the paddle 
+//                          (x, y, w, h, color)
+player = new gameObject(10, 400 , 30, 150, "#ff00f2");
+player.vx = 0;
+player.vy = 0;
+
+player2 = new gameObject(1010, 400 , 30, 150, "#4245ff");
+player2.vx = 0;
+player2.vy = 0;
+
+// the ball
+ball = new Ball(canvas, context);
+ball.moveBall();
+timer = setInterval(animate, interval);
+
+function animate()
+{
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        //player 1
+        if (w)
+        {
+                player.y -= 4;
+        }
+
+        if (s)
+        {
+                player.y += 4;
+        }
+        //player 2
+        if (38)
+        {
+                player2.y -= 4;
+        }
+
+        if (40)
+        {
+                player2.y += 4;
+        }
+        
+        boundry();
+
+        if (ball.collisionCheck(player))
+        {
+                ball.x = player.right() + ball.radius;
+                ball.color = "#615316";
+
+                if (ball.y < player.y - (player.height / 6))
+                {
+                        ball.speedX = 5;
+                        ball.speedY = -5;
+                }
+                else if (ball.y > player.y + (player.height / 6))
+                {
+                        ball.speedX = 5;
+                        ball.speedY = 5;
+                }
+                else
+                {
+                        ball.speedX = 5;
+                        ball.speedY = 0;
+                }
+        }
+
+        player.drawRect();
+        player.move();
+
+        player2.drawRect();
+        player2.move();
+
+        ball.drawBall();
+        ball.update();
+}
+
+
+function boundry()
+{
+    if (player.y > canvas.height - player.height / 2)
+    {
+        player.y = canvas.height - player.height / 2;
+    }
+
+    if (player.y < player.height / 2)
+    {
+        player.y = player.height / 2;
+    }
+}
